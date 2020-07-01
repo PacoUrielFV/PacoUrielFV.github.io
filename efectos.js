74c4c25bba854c72578cbe3024e1c68b519e2d33
Paco Flores <pacoflores1995@gmail.com>
2020-07-01 12:11:50 -0500

    Actualizacion

--- efectos.js
+++ efectos.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+
+    // Efecto Menu
+    $('.menu a').each(function(index, elemento){
+        $(this).css({
+            'top': '-200px'
+        });
+
+        $(this).animate({
+            'top': '0'
+        }, 2000 + (index * 500));
+    });
+// Efecto Header
+    if ($(window).width() > 800 ){
+        $('header .textos').css({
+            opacity:0,
+            marginTop: 0
+        });
+        $('header .textos').animate({
+            opacity:1,
+            marginTop: '-52px'
+        },1500 );
+    }
 // Scroll elementos menu
         var nuestroProducto = $('#nuestro-producto').offset().top,
             portafolio = $('#imagenes').offset().top;
